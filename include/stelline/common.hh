068c9957efa7d3a819905b77e09d7b30877157bf
Luigi Cruz <github@luigi.ltd>
2024-08-13 20:29:14 -0700

    Add generic CUDA error checker.

--- include/stelline/common.hh
+++ include/stelline/common.hh
@@ -6,6 +6,16 @@ namespace stelline {
 #define STELLINE_API __attribute__((visibility("default")))
 #define STELLINE_HIDDEN __attribute__((visibility("hidden")))
 
+#ifndef STELLINE_CUDA_CHECK_THROW
+#define STELLINE_CUDA_CHECK_THROW(f, callback) { \
+    const cudaError_t error = call; \
+    if (err != cudaSuccess) { \
+        callback(); \
+        throw std::runtime_error("[CUDA] Error Code: " + std::string(cudaGetErrorString(err))); \
+    } \
+}
+#endif  // STELLINE_CUDA_CHECK_THROW
+
 }  // namespace stelline
 
 #endif  // STELLINE_COMMON_HH
