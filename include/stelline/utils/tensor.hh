57c3d0a7880969fafe568c23f7ff60f1465b78e5
Luigi Cruz <github@luigi.ltd>
2026-02-13 22:32:41 +0000

    Add tensor creation tool for multiple dtypes.

--- include/stelline/utils/tensor.hh
+++ include/stelline/utils/tensor.hh
@@ -1,6 +1,8 @@
 #ifndef STELLINE_UTILS_TENSOR_HH
 #define STELLINE_UTILS_TENSOR_HH
 
+#include <string>
+
 #include <holoscan/holoscan.hpp>
 #include <matx.h>
 
@@ -17,6 +19,16 @@ std::shared_ptr<holoscan::Tensor> MakeBlockTensor(const BlockShape& shape) {
             static_cast<int64_t>(shape.numberOfSamples),
             static_cast<int64_t>(shape.numberOfPolarizations)
         }, matx::MATX_DEVICE_MEMORY).ToDlPack());
+}
+
+inline std::shared_ptr<holoscan::Tensor> MakeBlockTensor(const BlockShape& shape, const std::string& dtype) {
+    if (dtype == "cf32") {
+        return MakeBlockTensor<cuda::std::complex<float>>(shape);
+    }
+    if (dtype == "ci8") {
+        return MakeBlockTensor<cuda::std::complex<int8_t>>(shape);
+    }
+    throw std::runtime_error(fmt::format("Unsupported dtype: {}", dtype));
 }
 
 }  // namespace stelline
